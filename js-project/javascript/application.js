e3759ce017460e3151afa1a336aa5754436bc22f
Yann Klein <yann.klein@me.com>
2024-11-23 11:27:51 +0900

    lectuer done

--- js-project/javascript/application.js
+++ js-project/javascript/application.js
@@ -4,9 +4,9 @@ console.log("Hello from application.js!");
 // Select DOM elements
 // ////////
 
-// with its ID (Shinji)
-const shinji = document.querySelector("#shinji");
-console.log(shinji);
+// with its ID (Ruben)
+const ruben  = document.querySelector("#ruben-hedstrom");
+console.log(ruben);
 
 // with its tag type (h2)
 const title = document.querySelector("h2");
@@ -22,6 +22,7 @@ console.log(list);
 
 // select multiple elements (all the card)
 // cards is a NodeList, looks like an array
+
 const cards = document.querySelectorAll(".card");
 console.log(cards);
 
@@ -29,22 +30,19 @@ console.log(cards);
 // Modify the DOM
 // ////////
 
-// add an HTML element (Minato reds ⛑️)
-list.insertAdjacentHTML("beforeend", "<li>Minato reds ⛑️</li>");
-
-// same but more complex (add Vincci!!!)
+// add an HTML element (add Jasmine!!!)
 const yokoCard = document.querySelector(".card ul");
 yokoCard.insertAdjacentHTML(
-  "beforeend", 
+  "beforeend",
   `<li>
-    <img id="kam-ying-leung" src="https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1696212707/our5e5ylxkwblhpgnwlh.jpg" alt="">
-    <p>Kam Ying Leung</p>
+    <img id="jasmine-stivers" src="https://avatars.githubusercontent.com/u/178494344?v=4" alt="">
+    <p>Jasmine Stivers</p>
   </li>`
 );
 
 // change the element style (group background to green)
 // Careful! background-color => backgroundColor
-// group.style.color = "red";
+// group.style.color = "red"; // color: red;
 group.style.backgroundColor = "green";
 
 // add a class to the element (add text-white to group)
@@ -53,18 +51,15 @@ group.classList.add("text-white");
 
 // remove it
 group.classList.remove("text-white");
-group.classList.toggle("text-white");
-
-
 
 // Read/Write for inputs (change the value in the email input)
 const email = document.querySelector("#email");
 console.log(email.value);
-email.value = "yann@lewagon.org";
+email.value = "Hi!";
 
 // Read/Write the (inner) text and HTML content (Yokohama FC ⚽️ title)
-const yokoTitle = document.querySelector("h3");
+const yokoTitle = document.querySelector("#yoko");
 console.log(yokoTitle.innerText);
 console.log(yokoTitle.innerHTML);
 
-yokoTitle.innerHTML= "Hello <em>everyone</em>";
+yokoTitle.innerHTML = "Hi <em> batch #1802 </em>";
